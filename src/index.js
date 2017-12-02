6ef30078a8a587761c4e4bb4d54339b121e76be6
MagicalTaterTot <shadowbox826@gmail.com>
2017-12-02 11:14:37 -0500

    installed babel and webpack, working on dev server

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Navbar from './src/js/navbar.js'
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Navbar/>
+    </div>
+  </BrowserRouter>
+)
